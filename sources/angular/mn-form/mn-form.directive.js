9ae47936789e7a10c1cbf51e0fba60df995ab9f5
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-08 13:44:37 -0200

    add default name to form

--- sources/angular/mn-form/mn-form.directive.js
+++ sources/angular/mn-form/mn-form.directive.js
@@ -2,18 +2,23 @@ angular
   .module('mn-form')
   .directive('mnForm', MnFormDirective)
 
-function MnFormDirective() {
+function MnFormDirective($compile) {
   return {
     restrict: 'E',
     link,
   }
 
   function link(scope, element, attributes) {
-    if (attributes.submit) {
-      const form = element.find('form')
+    const form = element.find('form')
+    const instanceIndex = Array.from(document.querySelectorAll('mn-form')).indexOf(element[0])
 
+    const formName = form.attr('name') || `form${instanceIndex}`
+    form.attr('name', formName)
+    $compile(form)(scope)
+
+    if (attributes.submit) {
       form.bind('submit', () => {
-        const valid = scope[attributes.name].$valid
+        const valid = scope[formName].$valid
 
         if (valid) {
           scope.$eval(attributes.submit)
